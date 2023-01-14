a38d8bbd74284a9eaa577fb72dd5597d2de9f491
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 19:53:30 +0300

    improved

--- DZ/del second figure/Program.cs
+++ DZ/del second figure/Program.cs
@@ -3,5 +3,5 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int first_number = number / 100;
 int third_number = number % 10;
-System.Console.Write(first_number);
-System.Console.WriteLine(third_number);
+int new_number = int.Parse(first_number.ToString() + third_number.ToString());
+System.Console.WriteLine(new_number);
