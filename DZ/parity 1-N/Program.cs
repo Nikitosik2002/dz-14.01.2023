f0d34edbfd8ba6d5ae4fee843ccc6df6b0f986fd
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 19:11:58 +0300

    parity 1-N

---
+++ DZ/parity 1-N/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i < number)
+{
+    if (i % 2 == 0)
+{
+    System.Console.WriteLine(i);
+    System.Console.WriteLine("");
+}
+i++;
+}
