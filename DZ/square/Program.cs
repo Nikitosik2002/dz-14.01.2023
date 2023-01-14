2e8048a598603102d3e4faf20ab4b0493ff6ef95
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 17:18:12 +0300

    square check

---
+++ DZ/square/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите первое число");
+int first_number = Convert.ToInt16(Console.ReadLine());
+ System.Console.WriteLine("Введите второе число");
+int second_number = Convert.ToInt16(Console.ReadLine());
+
+    if (first_number == second_number * second_number)
+    {
+        System.Console.WriteLine("Первое число является квадратом второго");
+    }
+    else
+    {
+        System.Console.WriteLine("Первое число не является квадратом второго");
+    }
