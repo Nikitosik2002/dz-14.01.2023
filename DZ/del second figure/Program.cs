92a6eac0f9623478683a3b4503fb21e828906865
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 19:40:14 +0300

    delite second figure

---
+++ DZ/del second figure/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int first_number = number / 100;
+int third_number = number % 10;
+System.Console.Write(first_number);
+System.Console.WriteLine(third_number);
