3576390c1a17bcff9dfab87ba9360b57dfe26f23
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 18:39:44 +0300

    the second and third digits of a three-digit number

---
+++ DZ/three digit number/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int last_number = number % 10;
+int second_number = number % 100;
+second_number = second_number / 10;
+System.Console.WriteLine("Третья цифра трехзначного числа = " + last_number);
+System.Console.WriteLine("Вторая цифра трехзначного числа = " + second_number);
