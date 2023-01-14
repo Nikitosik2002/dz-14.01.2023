f8f6fb55289362340216663f95beab3147549f24
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 18:13:09 +0300

    parity check

---
+++ DZ/parity/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Число четное");
+}
+
+else
+{
+    System.Console.WriteLine("Число не четное");
+}
