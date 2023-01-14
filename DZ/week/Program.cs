a5d853a4be7c5caa8a64ac047fc02e2bb494ee71
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 17:32:46 +0300

    day of the week

---
+++ DZ/week/Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.WriteLine("Введите номер дня недели от 1 до 7");
+int number = Convert.ToInt16(Console.ReadLine());
+
+if(number == 1)
+{
+    System.Console.WriteLine("Сегодня понедельник");
+}
+else if(number == 2)
+{
+    System.Console.WriteLine("Сегодня вторник");
+}
+else if(number == 3)
+{
+    System.Console.WriteLine("Сегодня среда");
+}
+else if(number == 4)
+{
+    System.Console.WriteLine("Сегодня четверг");
+}
+else if(number == 5)
+{
+    System.Console.WriteLine("Сегодня пятница");
+}
+else if(number == 6)
+{
+    System.Console.WriteLine("Сегодня суббота");
+}
+else 
+{
+    System.Console.WriteLine("Сегодня воскресенье");
+}
