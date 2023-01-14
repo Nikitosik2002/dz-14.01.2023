be5dc01612cc0c5f9bf4af2f40d0bca4f00020f9
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 22:44:17 +0300

    find out the multiplicity

---
+++ DZ/multiplicity/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите делимое");
+int divisible = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите делитьель");
+int divider = Convert.ToInt32(Console.ReadLine());
+
+if (divisible % divider == 0)
+{
+    System.Console.WriteLine("Делитель кратен делимому, ответ = " + divisible / divider);
+}
+else
+{
+    System.Console.WriteLine("Делитель не кратен делимому, остаток от деления = " + divisible % divider);
+}
