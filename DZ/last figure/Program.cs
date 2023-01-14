617675a6a8faafa0ac3ba132addc48f88300cddd
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 22:54:49 +0300

    checking the third digit

---
+++ DZ/last figure/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number / 100 >= 1 && number / 100 < 10)
+{
+int last_number = number % 10;
+System.Console.WriteLine("Последняя цифра трехзначного числа равна " + last_number);
+}
+else
+{
+    System.Console.WriteLine("Число не трехзначное");
+}
