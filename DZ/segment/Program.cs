cf31325cac835a844715e678d5f0a01e1a4ea032
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-14 19:29:33 +0300

    segment of biggest figure

---
+++ DZ/segment/Program.cs
@@ -0,0 +1,21 @@
+﻿var number = new Random().Next(10, 99);
+System.Console.WriteLine(number);
+
+int first_number = number / 10;
+int second_number = number % 10;
+
+if (first_number > second_number)
+{
+    System.Console.WriteLine("Большая цифра = " + first_number);
+}
+else if (second_number > first_number)
+{
+    System.Console.WriteLine("Большая цифра = " + second_number);
+}
+else
+{
+    System.Console.WriteLine("Большей цифры нет, цифры равны ");
+}
+
+
+
